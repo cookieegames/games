d42c3337a9dd83318a60e21f286d8e604d28d567
cookieegames <cookieditionn@gmail.com>
2024-10-10 08:08:48 -0500

    Create analytics.js

---
+++ tetrisgame/css/analytics.js
@@ -0,0 +1,24 @@
+(function() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-8FZJSQZCW3';
+    document.head.appendChild(script);
+  
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-8FZJSQZCW3');
+  })();
+  
+
+
+
+  /*other */
+  window.addEventListener('DOMContentLoaded', () => {
+    const script = document.createElement('script');
+    script.defer = true;
+    script.setAttribute('data-domain', 'thepizzaedition.github.io');
+    script.src = 'https://plausible.io/js/script.js';
+    document.body.appendChild(script);
+});
